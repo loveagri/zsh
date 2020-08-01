4b82e972d2b5c246178987640434b6e254c9cb2e
loveagri <1044531744@qq.com>
2020-08-01 21:37:38 +0700

    improve4

--- centos.sh
+++ centos.sh
@@ -13,3 +13,5 @@ sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/ins
 
 
 
+
+
--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -8,3 +8,10 @@ sed -i 's/robbyrussell/ys/g' $HOME/.zshrc
 sed -i 's/plugins=(git)/plugins=(git history history-substring-search node npm wd web-search last-working-dir zsh-autosuggestions vi-mode)/g' $HOME/.zshrc
 
 echo success
+
+cd
+
+wget https://mimosa-pudica.net/src/incr-0.2.zsh
+
+source incr*.zsh
+
